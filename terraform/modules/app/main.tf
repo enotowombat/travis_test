51b864abaee30186cd75a15d65afdfac6d7b0327
Vadim Myagkov <g0nz0@mail.ru>
2018-01-28 14:19:31 +0000

    travis test

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -59,7 +59,12 @@ resource "google_compute_firewall" "firewall_puma" {
 
   allow {
     protocol = "tcp"
-    ports    = ["9292", "80"]
+    ports    = ["9292"]
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
   }
 
   source_ranges = ["0.0.0.0/0"]
